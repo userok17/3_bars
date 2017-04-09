41135cf7a9ecd39dee48fe976e13cf2f456f7681
userok17 <admin@1rpi.ru>
2017-04-09 16:13:28 +0600

    Update bars.py
    Вынесены непонятные константы в переменные longitude и latitude. В 25 строке вызов фукции distance

--- bars.py
+++ bars.py
@@ -9,8 +9,6 @@ def load_data(filepath):
         return json.load(data_file)
 
 
-    
-
 def get_biggest_bar(json_data):
     dict_bar = max(json_data, key=lambda item: item['SeatsCount'])
     return dict_bar
@@ -21,12 +19,21 @@ def get_smallest_bar(json_data):
     return dict_bar
 
 
+def distance(item, longitude, latitude):
+    return math.sqrt((longitude - float(item['Longitude_WGS84']))**2 + (latitude - float(item['Latitude_WGS84']))**2)
+    
+
 def get_closest_bar(data, longitude, latitude):
-    return min(data, key=lambda item: math.sqrt((longitude - float(item['Longitude_WGS84']))**2 + (latitude - float(item['Latitude_WGS84']))**2))
+    return min(data, key=lambda item: distance(item, longitube, latitude))
+
 
 
 if __name__ == '__main__':
     json_data = load_data('data-2897-2016-11-23.json')
     print(get_biggest_bar(json_data))
     print(get_smallest_bar(json_data))
-    print(get_closest_bar(json_data, 37.6215879462381080, 55.7653669567739740))
+
+    longitude = 37.6215879462381080
+    latitude = 55.7653669567739740
+    
+    print(get_closest_bar(data=json_data, longitude=longitude, latitude=latitude))
