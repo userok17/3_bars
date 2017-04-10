75f673942e8d1f446715d65c04c08ff132a99941
userok <admin@1rpi.ru>
2017-04-11 00:25:52 +0500

    Обработка ввода
    Избавление от сложной обработки ввода; функции ask_json_data и ask_coordinate сделан в один экземпляр ask

--- bars.py
+++ bars.py
@@ -3,7 +3,6 @@ import math
 import os
 import sys
 
-
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
@@ -26,51 +25,42 @@ def distance(item, longitude, latitude):
     
 
 def get_closest_bar(data, longitude, latitude):
-    ''' Самый ближайший бар '''
     return min(data, key=lambda item: distance(item, longitude, latitude))
 
 
-def ask_json_data(question):
-    ''' Спросить путь до файла и вернуть json данные '''
-    answer = None
-    while True:
-        answer = input(question)
-        if answer == 'q':
-            sys.exit(0)
-        json_data = load_data(answer)
-        if json_data:
-            return json_data
-        else:
-            print('Путь до json файла указан не правильно. Попробуйте ввести снова.')
+def ask(question, type_value=str):
+    answer = input(question)
+    if not answer:
+        print('Ошибка: Значение не может быть пустым')
+        sys.exit(1)        
+    try:
+        answer = type_value(answer)
+    except ValueError:
+        print('Ошибка: Неправильно указано значение.')
+        sys.exit(1)
+    return answer
 
-def ask_coordinate(question):
-    ''' Спросить координаты '''
-    while True:
-        answer = input(question)
-        if answer == 'q':
-            sys.exit(0)
-        try:
-            answer = float(answer)
-        except ValueError:
-            print('Неправильно указано значение координата. Попробуйте заново.')
-        else:
-            return answer
     
-
+        
 def print_bar(json_data):
-        ''' Вывести информацию о баре '''
-        print('Название бара: {}'.format(json_data['Name']))
-        print('Адрес: {}'.format(json_data['Address']))
-        print('Количество мест: {}'.format(json_data['SeatsCount']))
-        print()
+    print('Название бара: {}'.format(json_data['Name']))
+    print('Адрес: {}'.format(json_data['Address']))
+    print('Количество мест: {}'.format(json_data['SeatsCount']))
+    print()
     
 def main():
     print('Программа Бары!\n')
     
-    json_data = ask_json_data('Введите путь до json файла или "q" для выхода из программы: ')
+    filepath = ask(question='Введите путь до json файла: ')
+    json_data = load_data(filepath)
     
-    longitude = ask_coordinate('Введите долготу c gps-координаты или "q" для выхода из программы: ')
-    latitude = ask_coordinate('Введите широту c gps-координаты или "q" для выхода из программы: ')
+    if not json_data:
+        print('Ошибка: Путь до json файла указан не правильно.')
+        sys.exit(0)
+    
+    longitude = ask(question='Введите долготу c gps-координаты: ', type_value=float)
+    latitude = ask(question='Введите широту c gps-координаты: ', type_value=float)
+    
     print()
     
     print('Самый маленький бар:')
